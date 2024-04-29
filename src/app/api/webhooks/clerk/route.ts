4fb8ca9734dcd62cad1918cc063bde06dcaef69e
Nursultan <[nursultankoshekbaev477@gmail.com]>
2024-04-29 14:50:16 +0500

    clerk id remove

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -8,7 +8,6 @@ import { createUser } from "@/lib/actions/user.action";
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
-
   if (!WEBHOOK_SECRET) {
     throw new Error(
       "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
@@ -61,7 +60,6 @@ export async function POST(req: Request) {
       evt.data;
 
     const user = {
-      clerkId: id,
       email: email_addresses[0].email_address,
       username: username!,
       firstName: first_name,
